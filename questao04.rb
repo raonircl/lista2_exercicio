935afe9897f39033c7846bbe35f6a1c1653a78a0
raonircl <raoni.afi@gmail.com>
2023-07-30 23:07:14 -0300

    media aritmetica

---
+++ questao04.rb
@@ -0,0 +1,13 @@
+n1 = 7
+n2 = 5
+n3 = 7
+
+media = (n1 + n2 + n3) / 3
+
+if media >= 7
+  puts "Aprovado!"
+elsif media < 7 && media >= 5
+  puts "Recuperação!"
+elsif media < 5
+  puts "Reprovado!"
+end
