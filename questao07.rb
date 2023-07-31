ceb95f4177efe21f0b0d6826e33e3cbbb2ac0801
raonircl <raoni.afi@gmail.com>
2023-07-31 17:29:56 -0300

    verificar data

---
+++ questao07.rb
@@ -0,0 +1,12 @@
+# n1 = gets.chomp.to_i
+# n2 = gets.chomp.to_i
+# n3 = gets.chomp.to_i
+n1 = 31
+n2 = 10
+n3 = 1
+
+if (n1 >= 1 && n1 <= 31) && (n2 >= 1 && n2 <= 12) && (n3 > 0)
+  puts "data válida"
+else
+  puts "data inválida"
+end
